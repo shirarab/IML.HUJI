c3024e3939eda58b5a180fe640fb87e70738eeae
shirarab <shirarab@cs.huji.ac.il>
2022-03-31 21:02:37 +0300

    first challenge

--- challenge/agoda_cancellation_estimator.py
+++ challenge/agoda_cancellation_estimator.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 from typing import NoReturn
+
+import sklearn
+
 from IMLearn.base import BaseEstimator
 import numpy as np
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+
+N_NEIGHBORS = 10
 
 
 class AgodaCancellationEstimator(BaseEstimator):
@@ -22,6 +29,8 @@ class AgodaCancellationEstimator(BaseEstimator):
 
         """
         super().__init__()
+        self._coef = None
+        self._clf = None
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
@@ -39,7 +48,8 @@ class AgodaCancellationEstimator(BaseEstimator):
         -----
 
         """
-        pass
+        self.classifier = KNeighborsClassifier(n_neighbors=N_NEIGHBORS)
+        self.classifier.fit(X, y)
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -55,7 +65,9 @@ class AgodaCancellationEstimator(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        return np.zeros(X.shape[0])
+        return self.classifier.predict(X)
+        # return self._clf.predict(X)
+        # return np.sign(X @ self._coef)
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -74,4 +86,8 @@ class AgodaCancellationEstimator(BaseEstimator):
         loss : float
             Performance under loss function
         """
-        pass
+        y_pred = self._predict(X)
+        n_samples = y.shape[0]
+        # loss is the amount of wrong classifications
+        # compare = y_pred * y
+        return np.sum(y != y_pred) / n_samples * 100
--- challenge/agoda_cancellation_prediction.py
+++ challenge/agoda_cancellation_prediction.py
@@ -1,11 +1,47 @@
+from IMLearn import BaseEstimator
 from challenge.agoda_cancellation_estimator import AgodaCancellationEstimator
-from IMLearn.utils import split_train_test
+# from IMLearn.utils import split_train_test
+import sklearn
 
 import numpy as np
 import pandas as pd
 
 
-def load_data(filename: str):
+def _to_date_number(features: pd.DataFrame, keys: list) -> None:
+    for field in keys:
+        features[field] = pd.to_datetime(features[field])
+        features[field] = features[field].apply(lambda x: x.value)
+
+
+def _to_day_of_week(features, full_data, keys):
+    for field in keys:
+        new_key = field + "_dayofweek"
+        features[new_key] = pd.to_datetime(full_data[field])
+        features[new_key] = features[new_key].apply(lambda x: x.dayofweek)
+
+
+def _add_new_cols(features, full_data):
+    _to_day_of_week(features, full_data, ["checkin_date", "checkout_date", "booking_datetime"])
+    features['stay_days'] = (pd.to_datetime(full_data['checkout_date'])
+                             - pd.to_datetime(full_data['checkin_date']))
+    features['stay_days'] = features['stay_days'].apply(lambda x: x.days)
+    features['days_till_vacation'] = (pd.to_datetime(full_data['checkin_date'])
+                                      - pd.to_datetime(full_data['booking_datetime']))
+    features['days_till_vacation'] = features['days_till_vacation'].apply(lambda x: x.days)
+    features['is_checkin_on_weekend'] = features['checkin_date_dayofweek'].apply(lambda x: x > 4)
+
+    # for title in ['checkout_date']:
+    #     del features[title]
+
+
+def _add_categories(features, full_data, titles):
+    for title in titles:
+        features = pd.concat((features, pd.get_dummies(full_data[title], drop_first=True)),
+                             axis=1)
+    return features
+
+
+def load_data(filename: str, isTest: bool):
     """
     Load Agoda booking cancellation dataset
     Parameters
@@ -20,16 +56,22 @@ def load_data(filename: str):
     2) Tuple of pandas.DataFrame and Series
     3) Tuple of ndarray of shape (n_samples, n_features) and ndarray of shape (n_samples,)
     """
-    # TODO - replace below code with any desired preprocessing
-    full_data = pd.read_csv(filename).dropna().drop_duplicates()
-    features = full_data[["h_booking_id",
-                          "hotel_id",
-                          "accommadation_type_name",
-                          "hotel_star_rating",
-                          "customer_nationality"]]
-    labels = full_data["cancellation_datetime"]
+    full_data = pd.read_csv(filename)  # .dropna().drop_duplicates()
+    good_fields = ["hotel_star_rating",
+                   "hotel_live_date",
+                   "guest_is_not_the_customer", "no_of_adults", "no_of_children", "no_of_extra_bed", "no_of_room"]
+    features = full_data[good_fields]
+    _add_new_cols(features, full_data)  # adding columns for the length of the stay, is weekend, day of week
+    features = _add_categories(features, full_data,
+                               ['accommadation_type_name', 'customer_nationality', 'hotel_country_code',
+                                'charge_option'])
 
-    return features, labels
+    # features["cancellation_datetime"].replace(np.nan, "", inplace=True)
+    _to_date_number(features, ["hotel_live_date"])
+    if not isTest:
+        labels = full_data["cancellation_datetime"].between("2015-07-02", "2055-09-30").astype(int)
+        return features, labels
+    return features
 
 
 def evaluate_and_export(estimator: BaseEstimator, X: np.ndarray, filename: str):
@@ -51,18 +93,31 @@ def evaluate_and_export(estimator: BaseEstimator, X: np.ndarray, filename: str):
         path to store file at
 
     """
-    pd.DataFrame(estimator.predict(X), columns=["predicted_values"]).to_csv(filename, index=False)
+    pred = estimator.predict(X)
+    pd.DataFrame(pred, columns=["predicted_values"]).to_csv(filename, index=False)
+
+
+def expand_to_train_data(test_data, train_columns):
+    for col in train_columns:
+        if col not in test_data.columns:
+            test_data[col] = np.zeros(np.size(test_data, axis=0))
+    for col in test_data.columns:
+        if col not in train_columns:
+            del test_data[col]
 
 
 if __name__ == '__main__':
     np.random.seed(0)
 
     # Load data
-    df, cancellation_labels = load_data("../datasets/agoda_cancellation_train.csv")
-    train_X, train_y, test_X, test_y = split_train_test(df, cancellation_labels)
-
+    df, cancellation_labels = load_data("../datasets/agoda_cancellation_train.csv", isTest=False)
+    train_X, test_X, train_y, test_y = sklearn.model_selection.train_test_split(df, cancellation_labels)
+    all_features_in_training_set = df.columns
     # Fit model over data
     estimator = AgodaCancellationEstimator().fit(train_X, train_y)
-
+    test_of_week1 = load_data("test_set_week_1.csv", isTest=True)
+    expand_to_train_data(test_of_week1, all_features_in_training_set)
     # Store model predictions over test set
-    evaluate_and_export(estimator, test_X, "id1_id2_id3.csv")
+    evaluate_and_export(estimator, test_of_week1, "206996761_212059679_211689765.csv")
+    print(f"Percent wrong classifications: {estimator.loss(test_X, test_y)}")
+
